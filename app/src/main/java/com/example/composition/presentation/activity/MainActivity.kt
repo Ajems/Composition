920bebe8170068ccde13f1e785ad9460303640b5
Ajems <danilkorenev2004@gmail.com>
2023-08-29 12:11:34 +0300

    added ViewBinding

--- app/src/main/java/com/example/composition/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/example/composition/presentation/activity/MainActivity.kt
@@ -3,10 +3,22 @@ package com.example.composition.presentation.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.composition.R
+import com.example.composition.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding: ActivityMainBinding
+        get() = _binding ?: throw RuntimeException("ActivityMainBinding is null")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
--- app/src/main/java/com/example/composition/presentation/fragment/ChooseLevelFragment.kt
+++ app/src/main/java/com/example/composition/presentation/fragment/ChooseLevelFragment.kt
@@ -6,14 +6,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.composition.R
+import com.example.composition.databinding.FragmentChooseLevelBinding
 
 class ChooseLevelFragment : Fragment() {
+
+    private var _binding: FragmentChooseLevelBinding? = null
+    private val binding: FragmentChooseLevelBinding
+        get() = _binding ?: throw RuntimeException("ChooseLevelFragment is null")
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_choose_level, container, false)
+    ): View {
+        _binding = FragmentChooseLevelBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
