aeebab5295a981ba170ee25546685a487137b62d
Ajems <danilkorenev2004@gmail.com>
2023-08-29 12:37:12 +0300

    added transition from WelcomeFragment to ChooseLevelFragment

--- app/src/main/java/com/example/composition/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/example/composition/presentation/activity/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.composition.presentation.activity
 
+import android.content.Context
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.composition.R
+import android.view.View
+import android.view.WindowManager
 import com.example.composition.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,10 +14,59 @@ class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding
         get() = _binding ?: throw RuntimeException("ActivityMainBinding is null")
 
+    private var statusBarHeight: Int? = null
+    private var navigationBarHeight: Int? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setFullScreen()
+    }
+
+    private fun setFullScreen() {
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+        increaseLayoutMargin()
+        setStatusBarUiVisibility()
+    }
+
+    private fun setStatusBarUiVisibility() {
+        if (isLightTheme(this)) {
+            val systemDecorColor =
+                (View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR or View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR)
+            window.decorView.systemUiVisibility = systemDecorColor
+        } else {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
+        }
+    }
+
+    private fun isLightTheme(context: Context): Boolean {
+        val currentNightMode = context.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+        return currentNightMode == Configuration.UI_MODE_NIGHT_NO
+    }
+
+    private fun increaseLayoutMargin() {
+        calculateStatusAndNavigationBar()
+        if(statusBarHeight != null && navigationBarHeight != null) {
+            binding.mainContainer.setPadding(
+                0,
+                statusBarHeight!!,
+                0,
+                navigationBarHeight!!
+            )
+        }
+    }
+
+    private fun calculateStatusAndNavigationBar() {
+        val resourceIdSB = resources.getIdentifier("status_bar_height", "dimen", "android")
+        val resourceIdNB = resources.getIdentifier("navigation_bar_height", "dimen", "android")
+        if (resourceIdSB > 0 && resourceIdNB > 0) {
+            statusBarHeight = resources.getDimensionPixelSize(resourceIdSB)
+            navigationBarHeight = resources.getDimensionPixelSize(resourceIdNB)
+        }
     }
 
     override fun onDestroy() {
